08d997d4334f369102b0cd1a712ee4e246eb9e98
Yifang Meng <45935106+YifangMeng@users.noreply.github.com>
2020-11-22 16:00:45 +0100

    Update BlockManager.cs

--- TrainingProjectLab_2048/BlockManager.cs
+++ TrainingProjectLab_2048/BlockManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -112,28 +112,202 @@ namespace TrainingProjectLab_2048
                     }
                 }
             }
+            int ZeroIndex = -1;
+            //Maximun  3 numbers need to put to the top
+            for (int i = 0; i < 4; i++)
+            {
+                if (list[i] == 0)
+                {
+                    if (ZeroIndex == -1)
+                        ZeroIndex = i;
+                }
+                else
+                {
+                    if (ZeroIndex != -1)
+                    {
+                        if (changedList.Contains(i))
+                        {
+                            changedList[changedList.IndexOf(i)] = ZeroIndex;
+                        }
+                        list[ZeroIndex] = list[i];
+                        list[i] = 0;
+                        ZeroIndex = -1;
+                        i = ZeroIndex + 1;
+                        changedList.Add(-1);
+                    }
+                }
+            }
 
             return changedList;
         }
-        public static bool MoveUp()
+        public static bool MoveUp(ref Block[,] blks, ref Block[,] OldBlocks, ref int score)
         {
-            return true;
+            Block[,] TempBlocks = new Block[4, 4];
+            InitBlocks(ref TempBlocks);
+            Copy(ref TempBlocks, ref blks);
+
+            bool BlocksChanged = false;
+            for (int col = 0; col < 4; col++)
+            {
+                List<int> ls = new List<int>();
+                for (int row = 0; row < 4; row++)
+                {
+                    ls.Add(blks[row, col].num);
+                    blks[row, col].Combined = false;
+                    blks[row, col].NewBlock = false;
+                }
+                List<int> ChangedList = CheckChangedBlocks(ref ls, ref score);
+                if (ChangedList.Count != 0) 
+                {
+                    BlocksChanged = true;
+                    int l_ls = ChangedList.Count;
+                    for (int i = 0; i < l_ls; i++)
+                    {
+                        if (ChangedList[i] != -1)
+                            blks[ChangedList[i], col].Combined = true;
+                    }
+                    int ls_k = 0;
+                    for (int row = 0; row < 4; row++) 
+                    {
+                        blks[row, col].num = ls[ls_k];
+                        ls_k++;
+                    }
+                    ls.Clear();
+                }
+            }
+
+            if (BlocksChanged == true)
+            {
+                Copy(ref OldBlocks, ref TempBlocks);
+            }
+            return BlocksChanged;
 
         }
-        public static bool MoveDown()
+        public static bool MoveDown(ref Block[,] blks, ref Block[,] OldBlocks, ref int score)
         {
-            return true;
+            Block[,] TempBlocks = new Block[4, 4];
+            InitBlocks(ref TempBlocks);
+            Copy(ref TempBlocks, ref blks);
 
+            bool BlocksChanged = false;
+            for (int col = 0; col < 4; col++)
+            {
+                List<int> ls = new List<int>();
+                for (int row = 3; row >= 0; row--)
+                {
+                    ls.Add(blks[row, col].num);
+                    blks[row, col].Combined = false;
+                    blks[row, col].NewBlock = false;
+                }
+                List<int> ChangedList = CheckChangedBlocks(ref ls, ref score);
+                if (ChangedList.Count != 0) 
+                {
+                    BlocksChanged = true;
+                    int l_ls = ChangedList.Count;
+                    for (int i = 0; i < l_ls; i++)
+                    {
+                        if (ChangedList[i] != -1)
+                            blks[3 - ChangedList[i], col].Combined = true;
+                    }
+                    int ls_k = 0;
+                    for (int row = 3; row >= 0; row--)
+                    {
+                        blks[row, col].num = ls[ls_k];
+                        ls_k++;
+                    }
+                }
+                ls.Clear();
+            }
+
+            if (BlocksChanged == true)
+            {
+                Copy(ref OldBlocks, ref TempBlocks);
+            }
+            return BlocksChanged;
         }
-        public static bool MoveLeft()
+        public static bool MoveLeft(ref Block[,] blks, ref Block[,] OldBlocks, ref int score)
         {
-            return true;
+            Block[,] TempBlocks = new Block[4, 4];
+            InitBlocks(ref TempBlocks);
+            Copy(ref TempBlocks, ref blks);
 
+            bool BlocksChanged = false;
+            for (int row = 0; row < 4; row++)
+            {
+                List<int> ls = new List<int>();
+                for (int col = 0; col < 4; col++)
+                {
+                    ls.Add(blks[row, col].num);
+                    blks[row, col].Combined = false;
+                    blks[row, col].NewBlock = false;
+                }
+                List<int> ChangedList = CheckChangedBlocks(ref ls, ref score);
+                if (ChangedList.Count != 0) 
+                {
+                    BlocksChanged = true;
+                    int l_ls = ChangedList.Count;
+                    for (int i = 0; i < l_ls; i++)
+                    {
+                        if (ChangedList[i] != -1)
+                            blks[row, ChangedList[i]].Combined = true;
+                    }
+                    int ls_k = 0;
+                    for (int col = 0; col < 4; col++)
+                    {
+                        blks[row, col].num = ls[ls_k];
+                        ls_k++;
+                    }
+                    ls.Clear();
+                }
+            }
+
+            if (BlocksChanged == true)
+            {
+                Copy(ref OldBlocks, ref TempBlocks);
+            }
+            return BlocksChanged;
         }
-        public static bool MoveRight()
+        public static bool MoveRight(ref Block[,] blks, ref Block[,] OldBlocks, ref int score)
         {
-            return true;
+            Block[,] TempBlocks = new Block[4, 4];
+            InitBlocks(ref TempBlocks);
+            Copy(ref TempBlocks, ref blks);
 
+            bool BlocksChanged = false;
+            for (int row = 0; row < 4; row++)
+            {
+                List<int> ls = new List<int>();
+                for (int col = 3; col >= 0; col--)
+                {
+                    ls.Add(blks[row, col].num);
+                    blks[row, col].Combined = false;
+                    blks[row, col].NewBlock = false;
+                }
+                List<int> ChangedList = CheckChangedBlocks(ref ls, ref score);
+                if (ChangedList.Count != 0) 
+                {
+                    BlocksChanged = true;
+                    int l_ls = ChangedList.Count;
+                    for (int i = 0; i < l_ls; i++)
+                    {
+                        if (ChangedList[i] != -1)
+                            blks[row, 3 - ChangedList[i]].Combined = true;
+                    }
+                    int ls_k = 0;
+                    for (int col = 3; col >= 0; col--)
+                    {
+                        blks[row, col].num = ls[ls_k];
+                        ls_k++;
+                    }
+                    ls.Clear();
+                }
+            }
+
+            if (BlocksChanged == true)
+            {
+                Copy(ref OldBlocks, ref TempBlocks);
+            }
+            return BlocksChanged;
         }
 
 
