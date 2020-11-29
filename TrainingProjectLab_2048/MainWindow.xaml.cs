85535ae3571f25b4c8542610a528119c02398ab9
Yifang Meng <45935106+YifangMeng@users.noreply.github.com>
2020-11-29 16:19:55 +0100

    Update MainWindow.xaml.cs

--- TrainingProjectLab_2048/MainWindow.xaml.cs
+++ TrainingProjectLab_2048/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,39 +21,128 @@ namespace TrainingProjectLab_2048
     /// </summary>
     public partial class MainWindow : Window
     {
+        int score, PrevScore;
+        Block[,] blks = new Block[4, 4];
+        Block[,] OldBlks = new Block[4, 4];
+        Button[,] Btns = new Button[4, 4];
+        DoubleAnimation doubleAnimation;
+        Storyboard storyboard;
+
         public MainWindow()
         {
             InitializeComponent();
+            doubleAnimation = new DoubleAnimation();
+            //transition of font size
+            doubleAnimation.From = 88;
+            doubleAnimation.To = 40;
+            doubleAnimation.Duration = TimeSpan.FromMilliseconds(280);
+            storyboard = new Storyboard();
+            storyboard.Children.Add(doubleAnimation);
+            NewGame();
         }
 
         public void NewGame()
         {
+            score = 0;
+            BlockManager.InitNewGameBlocks(ref blks);
+            BlockManager.InitBlocks(ref OldBlks);
+            BlockManager.Copy(ref OldBlks, ref blks);
+            Score.Text = score.ToString();
+            RenderBoard();
 
         }
         public void BackGame()
         {
-
+            score = PrevScore;
+            BlockManager.Copy(ref blks, ref OldBlks);
+            Score.Text = score.ToString();
+            RenderBoard();
         }
-
-
+        public void RenderBoard()
+        {
+            //clear current board
+        }
+        public bool GameOver(Block[,] blks)
+        {
+            //along the same row, two blocks own the same value
+            for (int row = 0; row < 4; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    if (blks[row, col].num == blks[row, col + 1].num)
+                        return false;
+                }
+            }
+            //along the same column, two rows own the same value
+            for (int col = 0; col < 4; col++)
+            {
+                for (int row = 0; row < 3; row++)
+                {
+                    if (blks[row + 1, col].num == blks[row, col].num)
+                        return false;
+                }
+            }
+            //there are empty blocks left
+            for (int row = 0; row < 4; row++)
+            {
+                for (int col = 0; col < 4; col++)
+                {
+                    if (blks[row, col].num == 0)
+                        return false;
+                }
+            }
+            return true;
+        }
 
         public void MoveUp()
         {
-
+            PrevScore = score;
+            if (BlockManager.MoveUp(ref blks, ref OldBlks, ref score) == true)  
+            {
+                BlockManager.GenerateABlock(ref blks);
+                RenderBoard();
+                Score.Text = score.ToString();
+            }
         }
         public void MoveDown()
         {
-
+            PrevScore = score;
+            if (BlockManager.MoveDown(ref blks, ref OldBlks, ref score) == true) 
+            {
+                BlockManager.GenerateABlock(ref blks);
+                RenderBoard();
+                Score.Text = score.ToString();
+            }
         }
         public void MoveLeft()
         {
-
+            PrevScore = score;
+            if (BlockManager.MoveLeft(ref blks, ref OldBlks, ref score) == true) 
+            {
+                BlockManager.GenerateABlock(ref blks);
+                RenderBoard();
+                Score.Text = score.ToString();
+            }
         }
         public void MoveRight()
         {
-
+            PrevScore = score;
+            if (BlockManager.MoveRight(ref blks, ref OldBlks, ref score) == true)
+            {
+                BlockManager.GenerateABlock(ref blks);
+                RenderBoard();
+                Score.Text = score.ToString();
+            }
         }
-        private void Mgrid_KeyDown(object sender, KeyEventArgs e)
+        
+
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            //todo
+            NewGame();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
         {
             switch (e.Key)
             {
@@ -71,12 +161,11 @@ namespace TrainingProjectLab_2048
                 default:
                     break;
             }
-        }
+            if (GameOver(blks))
+            {
+                MessageBox.Show("Game over! Score: " + score.ToString(), "Notification");
+            }
 
-        private void New_Click(object sender, RoutedEventArgs e)
-        {
-            //todo
-            NewGame();
         }
 
         private void Back_Click(object sender, RoutedEventArgs e)
